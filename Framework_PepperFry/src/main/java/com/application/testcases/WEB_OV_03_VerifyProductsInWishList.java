b81f50e830f1af37f88c022a2febf1fb8fd348a3
srujanee077 <srujanee077@gmail.com>
2021-02-28 20:58:46 +0530

    Update WEB_OV_03_VerifyProductsInWishList.java
    removed Id and password in Login Section

--- Framework_PepperFry/src/main/java/com/application/testcases/WEB_OV_03_VerifyProductsInWishList.java
+++ Framework_PepperFry/src/main/java/com/application/testcases/WEB_OV_03_VerifyProductsInWishList.java
@@ -15,7 +15,7 @@ public class WEB_OV_03_VerifyProductsInWishList {
 	public void verifyProductsInWishList() throws InterruptedException {
 		driver =BrowserFactory.launchApplication(driver,"Chrome","https://www.pepperfry.com/");
 		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-		loginPage.LogInToApp("srujanee077@gmail.com", "SRuj@nee077");
+		loginPage.LogInToApp("", "");
 		SearchItem searchProduct = PageFactory.initElements(driver, SearchItem.class);
 		//searchProduct.searchHoverAddWishList("Furniture");
 		searchProduct.searchHoverAddWishList("3 Seater Sofas");
